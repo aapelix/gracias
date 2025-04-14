1a3948e78d8aab56372730b15f1ab6ae93cbbeac
aapelix <mcaapelix@gmail.com>
2025-04-14 14:43:46 +0300

    corrected the view box w and h for 24x24 icons

--- scripts/generateTypes.mjs
+++ scripts/generateTypes.mjs
@@ -12,31 +12,27 @@ async function convertIcons(directoryPath) {
       const filePath = path.join(directoryPath, file);
       let content = await fs.readFile(filePath, "utf8");
 
-      console.log(`Processing file: ${file}`); // Log the current file being processed
+      console.log(`Processing file: ${file}`);
 
-      // Broadly match component definitions
       const componentNameMatch = content.match(
         /(?:const|function|class)\s+(Svg[A-Za-z0-9]+)\s*[:=]/,
       );
       if (!componentNameMatch) {
-        console.log(`No component name found for file: ${file}`); // Log when no match is found
+        console.log(`No component name found for file: ${file}`);
         continue;
       }
 
       const componentName = componentNameMatch[1];
       const interfaceName = `${componentName}Props`;
-
-      // Log the component name
       console.log(`Found component name: ${componentName}`);
 
-      // Add the viewBox if it's missing
-      const originalContent = content; // Store the original content for comparison
+      const originalContent = content;
       content = addViewBoxToSvg(content);
 
       if (originalContent !== content) {
-        console.log(`Added viewBox to file: ${file}`); // Log if the viewBox was added
+        console.log(`Added viewBox to file: ${file}`);
       } else {
-        console.log(`No viewBox needed for file: ${file}`); // Log if no change was made
+        console.log(`No viewBox needed for file: ${file}`);
       }
 
       const newContent = `import * as React from "react";
@@ -49,7 +45,7 @@ const ${componentName} = React.forwardRef<SVGSVGElement, ${interfaceName}>(
       xmlns="http://www.w3.org/2000/svg"
       width="1em"
       height="1em"
-      viewBox="0 0 12 12"
+      viewBox="0 0 24 24"
       fill="currentColor"
       shapeRendering="crispEdges"
       ref={ref}
@@ -65,7 +61,6 @@ ${componentName}.displayName = "${componentName}";
 export default ${componentName};
 `;
 
-      // Write the new content back to the file
       await fs.writeFile(filePath, newContent, "utf8");
       console.log(`Converted: ${file}`);
     }
@@ -94,14 +89,12 @@ function addViewBoxToSvg(content) {
   const svgTagMatch = content.match(/<svg([^>]*)>/);
   if (svgTagMatch) {
     if (!svgTagMatch[1].includes("viewBox")) {
-      content = content.replace("<svg", '<svg viewBox="0 0 12 12"');
+      content = content.replace("<svg", '<svg viewBox="0 0 24 24"');
     }
   }
   return content;
 }
 
-// Replace with your icons directory path
-const iconsDirectory = "./24"; // Make sure this path is correct
+const iconsDirectory = "./24";
 
-// Call the function
 convertIcons(iconsDirectory);
